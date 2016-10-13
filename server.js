064bd4f5d6b6495996d8facfb8ea6c831aa00f92
Phil Boothe <pfb3cn@virginia.edu>
2016-10-13 14:53:40 -0400

    expanded on file architecture

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-var mongoose = require( 'mongoose' ),
-    express  = require( 'express' ),
-    bp       = require('body-parser'),
-    path     = require( 'path' ),
-    root     = __dirname,
-    port     = process.env.PORT || 8000,
-    app      = express(),
-    routes = require('./server/config/routes.js')
-app.use( express.static( path.join( root, 'client/views' )));
-app.use( express.static( path.join( root, 'bower_components' )));
-app.use(bp.json())
+var express = require('express'),
+    bp = require('body-parser'),
+    path = require('path'),
+    app = express(),
+    port = process.env.PORT || 8000;
+
+app.use(bp.urlencoded({ extended:true }));
+app.use(express.static(path.join(__dirname, "./client/static")));
+
+app.set('views', path.join(__dirname, './client/views'));
+app.set('view engine', 'ejs');
+
 require('./server/config/mongoose.js');
-routes(app);
-app.listen( port, function() {
-  console.log( `server running on port ${ port }` );
-});
+
+app.get('/', function(req, res) { res.render("index"); });
+
+var server = app.listen(port, function() { console.log(`server running on port ${ port }`); });
+var sockets = require('./server/controllers/sockets.js');
+sockets(server);
