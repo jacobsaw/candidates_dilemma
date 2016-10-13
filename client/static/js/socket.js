dc6f7119feef911b9454edf87d3c85fe08dbb241
Marco Molina <marcomolina11@gmail.com>
2016-10-13 17:42:20 -0400

    Up to date style.css and index.ejs

--- client/static/js/socket.js
+++ client/static/js/socket.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     var name = prompt("Please enter your name:");
     if (!name) {
-        $('body').html('<h1>Please enter a valid name.</h1>')
+        $('#middle_div').html('<h1>Please enter a valid name.</h1>')
     } else {
         var socket = io.connect();
         socket.emit('user_login', { name:name });
