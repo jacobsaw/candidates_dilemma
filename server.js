3d8203513921990f010f2a9618ee643ca6e8b33f
jacobsaw <adam.w.jacobs@gmail.com>
2016-10-13 14:14:42 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+var mongoose = require( 'mongoose' ),
+    express  = require( 'express' ),
+    bp       = require('body-parser'),
+    path     = require( 'path' ),
+    root     = __dirname,
+    port     = process.env.PORT || 8000,
+    app      = express(),
+    routes = require('./server/config/routes.js')
+app.use( express.static( path.join( root, 'client/views' )));
+app.use( express.static( path.join( root, 'bower_components' )));
+app.use(bp.json())
+require('./server/config/mongoose.js');
+routes(app);
+app.listen( port, function() {
+  console.log( `server running on port ${ port }` );
+});
