1e5c4b22beaa5c1ee75d634ba43b9caf69f0a97c
Phil Boothe <pfb3cn@virginia.edu>
2016-10-26 16:53:50 -0400

    updated content in partials

---
+++ client/assets/controllers/resultController.js
@@ -0,0 +1,10 @@
+app.controller('resultController', ['$scope', 'candidateFactory', function($scope, candidateFactory) {
+    $scope.candidates = [];
+
+    function getCandidates() {
+        candidateFactory.index(function(data) {
+            $scope.candidates = data;
+        });
+    }
+    getCandidates();
+}])
